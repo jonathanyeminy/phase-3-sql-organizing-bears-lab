2bebdc7f80cc89138f1e6e44a0a6e6fa6a4b6d6c
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-05-03 19:57:06 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'ornery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'F', 'brown', 'ornery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 1, 'F', 'black', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', 'brown', 'ornery', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'M', 'brown', 'ornery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'brown', 'ornery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'brown', 'ornery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'brown', 'ornery', true);
